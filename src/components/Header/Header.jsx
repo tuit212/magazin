2f6c36b470ef220da7939668f4ccd4b75632f371
AsadbekAnvarov2305 <asadbekanvarov2305@gmail.com>
2023-02-15 23:36:42 +0500

    firist commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from '../../Assets/Img/site-logo.svg';
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className='site-header'>
+        <div className='container'>
+            <div className='header'>
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
